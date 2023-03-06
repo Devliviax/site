67fa83de51808f97ecfdddd19ad3d1523b2ad0cf
Lívia Xavier <104855077+Devliviax@users.noreply.github.com>
2023-03-06 00:12:06 -0300

    first

---
+++ web/src/js/texto.js
@@ -0,0 +1,1 @@
+function diminuirLetra(){var e=(e=document.getElementById("Conteudo").style.fontSize).replace("px","");document.getElementById("Conteudo").style.fontSize=""==e?"11px":parseInt(e)-1+"px"}function aumentarLetra(){var e=(e=document.getElementById("Conteudo").style.fontSize).replace("px","");document.getElementById("Conteudo").style.fontSize=""==e?"13px":parseInt(e)+1+"px"}
