5eddcbcec0d1f411527373dcda8cad1f47936b11
niljoshua <niljoshuaa@gmail.com>
2023-03-06 02:36:34 -0300

    atualizacao

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
-window.onscroll=()=>{myFunction()};var navbar=document.getElementById("navbar"),sticky=navbar.offsetTop;function myFunction(){1<window.scrollY?navbar.classList.add("sticky"):navbar.classList.remove("sticky")}var elementBody=document.querySelector("body"),btnAumentar=document.querySelector("#btnaumentar"),btnDiminuir=document.querySelector("#btndiminuir"),fontSize=100,increaseDecrease=10;btnDiminuir.addEventListener("click",function(e){fontSize-=increaseDecrease,elementBody.style.fontSize=fontSize+"%"}),btnAumentar.addEventListener("click",function(e){fontSize+=increaseDecrease,elementBody.style.fontSize=fontSize+"%"});
+window.onscroll = () => {
+    myFunction()
+};
+var navbar = document.getElementById("navbar"),
+    sticky = navbar.offsetTop;
+
+function myFunction() {
+    1 < window.scrollY ? navbar.classList.add("sticky") : navbar.classList.remove("sticky")
+}
+var elementBody = document.querySelector("body"),
+    btnAumentar = document.querySelector("#btnaumentar"),
+    btnDiminuir = document.querySelector("#btndiminuir"),
+    fontSize = 100,
+    increaseDecrease = 10;
+btnDiminuir.addEventListener("click", function (e) {
+    fontSize -= increaseDecrease, elementBody.style.fontSize = fontSize + "%"
+}), btnAumentar.addEventListener("click", function (e) {
+    fontSize += increaseDecrease, elementBody.style.fontSize = fontSize + "%"
+});
--- src/js/texto.js
+++ src/js/texto.js
@@ -1,1 +1,9 @@
-function diminuirLetra(){var e=(e=document.getElementById("Conteudo").style.fontSize).replace("px","");document.getElementById("Conteudo").style.fontSize=""==e?"11px":parseInt(e)-1+"px"}function aumentarLetra(){var e=(e=document.getElementById("Conteudo").style.fontSize).replace("px","");document.getElementById("Conteudo").style.fontSize=""==e?"13px":parseInt(e)+1+"px"}
+function diminuirLetra() {
+    var e = (e = document.getElementById("Conteudo").style.fontSize).replace("px", "");
+    document.getElementById("Conteudo").style.fontSize = "" == e ? "11px" : parseInt(e) - 1 + "px"
+}
+
+function aumentarLetra() {
+    var e = (e = document.getElementById("Conteudo").style.fontSize).replace("px", "");
+    document.getElementById("Conteudo").style.fontSize = "" == e ? "13px" : parseInt(e) + 1 + "px"
+}
